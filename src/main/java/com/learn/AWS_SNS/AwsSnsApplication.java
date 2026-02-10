0f021f7ae74f0a4227a48fb53bed6c8de51a07fd
mauryanitish <m.nitishkumar786@gmail.com>
2026-02-10 12:45:14 +0530

    Initial commit

---
+++ src/main/java/com/learn/AWS_SNS/AwsSnsApplication.java
@@ -0,0 +1,13 @@
+package com.learn.AWS_SNS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/AWS_SNS/AwsSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.AWS_SNS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
